430ab897d7c8d58d60fa3fec37ffdbc75bc6cb0c
Karan Dodeja <41475214+Karan-Dodeja@users.noreply.github.com>
2024-12-23 03:45:36 +0530

    Setup Live Blocks Provider

--- figma/src/app/dashboard/[id]/page.tsx
+++ figma/src/app/dashboard/[id]/page.tsx
@@ -7,9 +7,8 @@ export default async function Page({ params }: { params: ParamsType }) {
   const session = await auth();
 
   return (
-    <></>
-    // <Room>
-    //     <Canvas></Canvas>
-    // </Room>
-  )
+    <Room roomId={"room" + id}>
+      // <Canvas></Canvas>
+    </Room>
+  );
 }
---
+++ figma/src/components/liveblocks/Room.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { LiveList, LiveMap, LiveObject } from "@liveblocks/client";
+import { LiveblocksProvider, RoomProvider } from "@liveblocks/react";
+import { ReactNode } from "react";
+import { Layer } from "~/types";
+
+export function Room({
+  children,
+  roomId,
+}: {
+  children: ReactNode;
+  roomId: string;
+}) {
+  return (
+    <LiveblocksProvider authEndpoint="">
+      <RoomProvider
+        id={roomId}
+        initialPresence={{
+          selection: [],
+          cursor: null,
+          penColor: null,
+          pencilDraft: null,
+        }}
+        initialStorage={{
+          roomColor: { r: 30, g: 30, b: 30 },
+          layers: new LiveMap<string, LiveObject<Layer>>(),
+          layerIds: new LiveList([]),
+        }}
+      >
+        <div>
+          <h1>Room: {roomId}</h1>
+          {children}
+        </div>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
